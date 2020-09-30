23cab54871c7daefccffde760959a80b1d654404
zhazgul004 <zhazgul004@gmail.com>
2020-09-30 16:07:39 +0600

    first commit

---
+++ bil301/hw2.cs
@@ -0,0 +1,9 @@
+using System;
+class HW {
+	static void Main() {
+		int x, y, z; x = 100; y = 1; z = 0;
+
+		Console.WriteLine((x/y != 0) && (x/z != 0));
+		//Console.WriteLine((x/y != 0) & (x/z != 0));
+	}
+}
