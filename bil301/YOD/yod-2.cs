1e5cc5b5d208b297ba7d5b013890ab0d13a30ba6
zhazgul004 <zhazgul004@gmail.com>
2020-10-06 18:46:01 +0600

    dsdszdf

--- bil301/YOD/yod-2.cs
+++ bil301/YOD/yod-2.cs
@@ -1,58 +1,45 @@
+/*
+tan(x) = 0
+    x = 0, 180, 360,...
+tan(x) = undef
+    x = 90, 270, 450...
+
+*/
+
 using System;
 
 class YOD2 {
     public static void Main() {
-        Console.WriteLine("Kancha sap maalymat kirgizuunu kaalaisyz?");
-
-        int n = Int32.Parse(Console.ReadLine());
-        String[] firma = new String[n];
-        int[] prog = new int[n];
-        long[] vol = new long[n];
-        double[] ul = new double[n];
-
-        for (int i = 0; i < n; i++) {
-            Console.WriteLine("Maalymatty kirgiziniz:\nFirma:");
-            firma[i] = Console.ReadLine();
-            Console.WriteLine("Programdyk karajattyn sany:");
-            prog[i] = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Satuunun jyldyk kolomu:");
-            vol[i] = Int64.Parse(Console.ReadLine());
-            Console.WriteLine("Rynok ulushu:");
-            ul[i] = Double.Parse(Console.ReadLine());
-            Console.WriteLine();
-        }
-
-
-        Console.WriteLine("|-----------------------------------------------------|");
-        Console.Write("| Ondurush firmalar".PadRight(54));
-        Console.WriteLine("|");
-        Console.WriteLine("|-----------------------------------------------------|");
-        Console.WriteLine("|          |Programdyk     |Satuunun jyldyk |Rynok    |");
-        Console.WriteLine("| Firma    |karajattyn sany|kolomu($)       |ulushu(%)|");
-        Console.WriteLine("|-----------------------------------------------------|");
-
+        double t1, t2, a = 30, x = 1, b = 1;
         
-        for (int i = 0; i < n; i++) {
-            Console.Write("|");
-            Console.Write(firma[i].PadRight(10));
-            
-            Console.Write("|");
-            Console.Write(prog[i].ToString().PadRight(15));
-            //Console.WriteLine(value.ToString(specifier, culture));
+        for (;;) {
+            Console.WriteLine("Enter value of a (except zero):");
+            a = Int32.Parse(Console.ReadLine());
+            if (a != 0) break;
+        }
 
-            Console.Write("|");
-            Console.Write(vol[i].ToString().PadRight(16));
-            
-            Console.Write("|");
-            Console.Write(ul[i].ToString().PadRight(9));
-            
-            Console.Write("|\n");
+        Console.WriteLine("Enter value of b:");
+        b = Int32.Parse(Console.ReadLine());
+        
+        for (;;) {
+            Console.WriteLine("Enter value of x (except zero, a*x != 90*k):");
+            x = Int32.Parse(Console.ReadLine());
+            if (x > 0 && (a*x % 90) != 0) break;
         }
 
 
-        Console.WriteLine("|-----------------------------------------------------|");
-        Console.Write("| Eskertuu: Gartner Group maalymaty boiuncha (2009 j)".PadRight(54));
-        Console.WriteLine("|");
-        Console.WriteLine("|-----------------------------------------------------|");
+        double rad = ConvertDegreesToRadians(a*x);
+        double rad2 = ConvertDegreesToRadians(a*x/2);
+
+        t1 = 2/(35*a*a) * (5*Math.Pow(x, 2.5) - 7*b*Math.Pow(x, 3.5));
+        t2 = Math.Sin(rad)/(2*Math.Pow(Math.Cos(rad),2)) + Math.Log(Math.Tan(rad2))/2;
+
+        Console.WriteLine("\nFor values a = {0}, b = {1}, x = {2}\nwe get result\nt1 = {3}\nt2 = {4}", a, b, x, t1, t2);
     }
+
+    public static double ConvertDegreesToRadians (double degrees)
+    {
+      double radians = (Math.PI / 180) * degrees;
+      return (radians);
+    } 
 }
