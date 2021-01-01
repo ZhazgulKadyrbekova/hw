26b25ad8ec38c72860e046a01466998b9fb6a4f8
zhazgul004 <zhazgul004@gmail.com>
2021-01-01 17:10:22 +0600

    dofinal'noe vremya

--- bil205/wind/src/Main.java
+++ bil205/wind/src/Main.java
@@ -3,80 +3,60 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame panel = new JFrame("My frame");
-        panel.setSize(500, 400);
-        panel.setLayout(null);
-        panel.setVisible(true);
+        JFrame frame = new JFrame("My frame");
+        frame.setSize(500, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
 
 
         JLabel name = new JLabel("What is your name?");
         name.setBounds(20, 20, 230, 20);
-        panel.add(name);
+        frame.add(name);
 
         JTextArea textArea = new JTextArea("Enter your name");
         textArea.setBounds(250, 20, 230, 20);
-        panel.add(textArea);
+        frame.add(textArea);
 
         JLabel surname = new JLabel("What is your surname?");
         surname.setBounds(20, 50, 230, 20);
-        panel.add(surname);
+        frame.add(surname);
 
         JTextField textField = new JTextField("Enter your surname");
         textField.setBounds(250, 50, 230, 20);
-        panel.add(textField);
+        frame.add(textField);
 
         JLabel genderLabel = new JLabel("What is your gender?");
         genderLabel.setBounds(20, 80, 230, 30);
-        panel.add(genderLabel);
+        frame.add(genderLabel);
 
-        String[] gender= { "Male", "Female", "Prefer not to say" };
-        JComboBox comboBox = new JComboBox(gender);
-        comboBox.setBounds(250, 80, 230, 30);
-        panel.add(comboBox);
+        JRadioButton male = new JRadioButton("Male");
+        JRadioButton female = new JRadioButton("Female");
+        male.setBounds(250, 80, 100, 30);
+        female.setBounds(370, 80, 100, 30);
+        frame.add(male);
+        frame.add(female);
 
         JLabel language = new JLabel("What is your favorite language?");
         language.setBounds(20, 120, 230, 70);
-        panel.add(language);
+        frame.add(language);
 
-        DefaultListModel<String> languages = new DefaultListModel<>();
-        languages.addElement("Java");
-        languages.addElement("Python");
-        languages.addElement("C");
-        languages.addElement("PHP");
-        JList<String> list = new JList<>(languages);
-        list.setBounds(250, 120, 230, 70);
-        panel.add(list);
+        JCheckBox java = new JCheckBox("Java");
+        JCheckBox c = new JCheckBox("C++");
+        JCheckBox python = new JCheckBox("Python");
+        JCheckBox php = new JCheckBox("PHP");
+        java.setBounds(250, 120, 230, 20);
+        c.setBounds(250, 140, 230, 20);
+        python.setBounds(250, 160, 230, 20);
+        php.setBounds(250, 180, 230, 20);
+        frame.add(java);
+        frame.add(c);
+        frame.add(python);
+        frame.add(php);
 
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("File");
-        JMenuItem item1 = new JMenuItem("New"),
-                item2 = new JMenuItem("Save"),
-                item3 = new JMenuItem("Exit");
-        menu.add(item1);
-        menu.add(item2);
-        menu.add(item3);
-        menuBar.add(menu);
-        panel.setJMenuBar(menuBar);
-
-        JOptionPane.showMessageDialog(panel, "Hello");
-
-        JLabel age = new JLabel("What is your age?");
-        age.setBounds(20, 200, 230, 100);
-        panel.add(age);
-
-        JScrollBar scrollBar = new JScrollBar();
-        scrollBar.setBounds(250, 200, 230, 100);
-        panel.add(scrollBar);
-
-        JPanel jPanel = new JPanel();
-        jPanel.setBounds(190, 310, 120, 70);
-        jPanel.setBackground(Color.cyan);
-        panel.add(jPanel);
 
         JButton button = new JButton("Submit");
-        button.setBounds(200, 320, 100, 50);
-//        jPanel.add(button);
-        panel.add(button);
+        button.setBounds(200, 220, 100, 50);
+        frame.add(button);
 
     }
 }
