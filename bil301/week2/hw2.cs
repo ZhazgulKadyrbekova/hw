6f71f6201ba27ac80a44e7dcb29a327c804bd4d1
zhazgul004 <zhazgul004@gmail.com>
2021-02-07 23:00:53 +0600

    test

--- bil301/week2/hw2.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class HW {
-	static void Main() {
-		int x, y, z; x = 100; y = 1; z = 0;
-
-		Console.WriteLine((x/y != 0) && (x/z != 0));
-		//Console.WriteLine((x/y != 0) & (x/z != 0));
-	}
-}
