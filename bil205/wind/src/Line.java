609b5911180fce212c0add9b30bfc5039a5eb1a3
zhazgul004 <zhazgul004@gmail.com>
2021-02-07 22:57:36 +0600

    After final changes

---
+++ bil205/wind/src/Line.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * This program demonstrates how to draw lines using Graphics2D object.
+ * @author www.codejava.net
+ *
+ */
+public class Line extends JFrame {
+
+    public Line() {
+        super("Lines Drawing Demo");
+
+        setSize(480, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    void drawLines(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.RED);
+        // creates a solid stroke with line width is 2
+        Stroke stroke = new BasicStroke(2f);
+        g2d.setStroke(stroke);
+        g2d.drawLine(20, 90, 100, 40);
+
+        g2d.setColor(Color.YELLOW);
+        g2d.drawLine(120, 50, 360, 50);
+
+        g2d.setColor(Color.GREEN);
+        g2d.setStroke(new BasicStroke(4f));
+        g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
+
+        g2d.setColor(Color.BLUE);
+        g2d.setStroke(new BasicStroke(6f));
+        g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
+
+
+//        g2d.drawLine(120, 50, 360, 50);
+//
+//        g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
+//
+//        g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
+
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawLines(g);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Line().setVisible(true);
+            }
+        });
+    }
+}
